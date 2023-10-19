cfa771ab247da8df2bd5af670ee9181d8ad87df2
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-10-19 12:41:13 +0900

    リファクタリング

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,42 +2,37 @@
 import minimist from "minimist";
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
-
-let month;
-if (isFinite(argv.m)) {
-  month = argv.m - 1;
-} else {
-  month = today.getMonth();
-}
+const month = isFinite(argv.m) ? argv.m - 1 : today.getMonth();
 const year = argv.y || today.getFullYear();
-const monthLastDay = new Date(year, month + 1, 0).getDate(); // 月は翌月、日の0は前月の最終日の意味
 
-console.log(`      ${month + 1}月 ${year}`);
-console.log("日 月 火 水 木 金 土");
+const printCal = (year, month) => {
+  const monthLastDay = new Date(year, month + 1, 0).getDate();
+  const monthFristDay = new Date(year, month, 1);
 
-let date = new Date(year, month, 1);
-if (date.getDay() !== 0) {
-  for (let i = 1; i <= date.getDay(); i++) {
-    process.stdout.write("   ");
-  }
-}
+  console.log(`      ${month + 1}月 ${year}`);
+  console.log("日 月 火 水 木 金 土");
 
-for (let i = 1; i <= monthLastDay; i++) {
-  let date = new Date(year, month, i);
-  let day = date.getDate().toString();
-
-  if (i < 10) {
-    if (date.getDay() == 6) {
-      console.log(`${day.padStart(2, " ")}`);
-    } else {
-      process.stdout.write(`${day.padStart(2, " ")} `);
+  if (monthFristDay.getDay() !== 0) {
+    for (let i = 1; i <= monthFristDay.getDay(); i++) {
+      process.stdout.write("   ");
     }
-  } else if (date.getDay() == 6) {
-    console.log(day);
-  } else {
-    process.stdout.write(`${date.getDate().toString()} `);
   }
-}
 
+  for (let i = 1; i <= monthLastDay; i++) {
+    let date = new Date(year, month, i);
+    let day = date.getDate().toString();
 
+    if (i < 10) {
+      process.stdout.write(day.padStart(2, " "));
+    } else {
+      process.stdout.write(day);
+    }
+    process.stdout.write(" ");
+    if (date.getDay() == 6) {
+      console.log("");
+    }
+  }
+  console.log("\n");
+};
 
+printCal(year, month);
