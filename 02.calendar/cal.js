0bfee179daf9a4884b425765b893cdad8a3a6a48
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-10-19 16:08:42 +0900

    今日の日付の反転を追加

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,36 +1,33 @@
 #!/usr/bin/env node
+
 import minimist from "minimist";
+
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
 const month = isFinite(argv.m) ? argv.m - 1 : today.getMonth();
 const year = argv.y || today.getFullYear();
 
 const printCal = (year, month) => {
-  const monthLastDay = new Date(year, month + 1, 0).getDate();
   const monthFristDay = new Date(year, month, 1);
+  const monthLastDay = new Date(year, month + 1, 0).getDate();
 
-  console.log(`      ${month + 1}月 ${year}`);
-  console.log("日 月 火 水 木 金 土");
+  console.log(`${" ".repeat(5)} ${month + 1}月 ${year}\n日 月 火 水 木 金 土`);
 
   if (monthFristDay.getDay() !== 0) {
-    for (let i = 1; i <= monthFristDay.getDay(); i++) {
-      process.stdout.write("   ");
-    }
+    process.stdout.write("   ".repeat(monthFristDay.getDay()));
   }
 
   for (let i = 1; i <= monthLastDay; i++) {
     let date = new Date(year, month, i);
     let day = date.getDate().toString();
 
-    if (i < 10) {
-      process.stdout.write(day.padStart(2, " "));
+    if (date.toDateString() === today.toDateString()) {
+      process.stdout.write(`\x1b[7m${day.padStart(2, " ")}\x1b[m `);
     } else {
-      process.stdout.write(day);
+      process.stdout.write(`${day.padStart(2, " ")} `);
     }
-    process.stdout.write(" ");
-    if (date.getDay() == 6) {
-      console.log("");
-    }
+
+    if (date.getDay() === 6) console.log("");
   }
   console.log("\n");
 };
