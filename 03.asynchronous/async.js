b34edab9cf2a857f21e3bdf7e190a046bb87ebc3
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-11-08 18:22:10 +0900

    async.jsを作成

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -1,38 +1,52 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
-async function f() {
-  console.log("1");
-  let promise = new Promise((resolve) => {
-    console.log("2");
+const runSqlToInsert = (sql) =>
+  new Promise((resolve, reject) => {
+    db.run(sql, function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        console.log(`ID${this.lastID}が挿入されました`);
+        resolve();
+      }
+    });
+  });
+
+const displayAll = (sql) =>
+  new Promise((resolve, reject) => {
+    db.all(sql, (error, rows) => {
+      if (error) {
+        reject(error);
+      } else {
+        rows.forEach((element) => console.log(element));
+        resolve();
+      }
+    });
+  });
+
+const createTable = () =>
+  new Promise((resolve, reject) => {
     db.run(
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(10) NOT NULL)",
-      () => resolve("DONE")
+      (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      }
     );
   });
-  await promise;
-  await function insertCherry() {
-    db.run('INSERT INTO books(title) VALUES("チェリー本")');
-  };
-  db.get(
-    "SELECT * FROM books WHERE rowid = last_insert_rowid()",
-    (_err, row) => {
-      console.log(row.id);
-    }
-  );
-  db.run('INSERT INTO books(title) VALUES("キウイ本")');
-  db.get(
-    "SELECT * FROM books WHERE rowid = last_insert_rowid()",
-    (_err, row) => {
-      console.log(row.id);
-    }
-  );
 
-  db.each("select * from books", (_err, row) => {
-    console.log(`id:${row.id} タイトル：${row.title}`);
-  });
+//エラーなし
 
-  // db.run("drop table if exists books")
+async function sequentialStart() {
+  await createTable();
+  await runSqlToInsert('INSERT INTO books(title) VALUES("チェリー本")');
+  await runSqlToInsert('INSERT INTO books(title) VALUES("ブルーベリー本")');
+  await displayAll("SELECT * FROM books");
+  db.run("drop table if exists books");
 }
 
-f();
+sequentialStart();
