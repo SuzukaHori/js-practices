c648ddf1d974637a4c2490ee4d471673639b8be4
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-11-10 20:37:19 +0900

    async関数の定義を削除
    関数を定義せずに Top-level awaitを使用するように変更しました。

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -5,62 +5,54 @@ import { runSql, runSqlToInsert, displayAll } from "./function.js";
 const db = new sqlite3.Database(":memory:");
 
 // エラーなし
-async function processSuccessfully() {
-  await runSql(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(10) NOT NULL UNIQUE)"
-  );
-  let id = await runSqlToInsert(
-    db,
-    "INSERT INTO books (title) VALUES ('チェリー本')"
-  );
-  console.log(`ID${id}の要素が追加されました`);
-  let id2 = await runSqlToInsert(
-    db,
-    "INSERT INTO books (title) VALUES ('ブルーベリー本')"
-  );
-  console.log(`ID${id2}の要素が追加されました`);
-  const rows = await displayAll(db, "SELECT * FROM books");
-  rows.forEach((row) => {
-    console.log(row);
-  });
-  await runSql(db, "DROP TABLE books");
-}
-
-processSuccessfully();
+await runSql(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(10) NOT NULL UNIQUE)"
+);
+let id = await runSqlToInsert(
+  db,
+  "INSERT INTO books (title) VALUES ('チェリー本')"
+);
+console.log(`ID${id}の要素が追加されました`);
+let id2 = await runSqlToInsert(
+  db,
+  "INSERT INTO books (title) VALUES ('ブルーベリー本')"
+);
+console.log(`ID${id2}の要素が追加されました`);
+const rows = await displayAll(db, "SELECT * FROM books");
+rows.forEach((row) => {
+  console.log(row);
+});
+await runSql(db, "DROP TABLE books");
 
 await timers.setTimeout(100);
 
 // エラーあり
-async function processWithErrors() {
-  await runSql(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(10) NOT NULL UNIQUE)"
-  );
-  let id = await runSqlToInsert(
-    db,
-    "INSERT INTO books (title) VALUES ('チェリー本')"
-  );
-  console.log(`ID${id}の要素が追加されました`);
-  try {
-    await runSqlToInsert(db, "INSERT INTO books (title) VALUES ('チェリー本')");
-  } catch (error) {
-    if (error.code === "SQLITE_CONSTRAINT") {
-      console.error(error.message);
-    } else {
-      throw error;
-    }
+await runSql(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(10) NOT NULL UNIQUE)"
+);
+let id3 = await runSqlToInsert(
+  db,
+  "INSERT INTO books (title) VALUES ('チェリー本')"
+);
+console.log(`ID${id3}の要素が追加されました`);
+try {
+  await runSqlToInsert(db, "INSERT INTO books (title) VALUES ('チェリー本')");
+} catch (error) {
+  if (error.code === "SQLITE_CONSTRAINT") {
+    console.error(error.message);
+  } else {
+    throw error;
   }
-  try {
-    await displayAll(db, "SELECT * FROM book");
-  } catch (error) {
-    if (error.code === "SQLITE_ERROR") {
-      console.error(error.message);
-    } else {
-      throw error;
-    }
-  }
-  await runSql(db, "DROP TABLE books");
 }
-
-processWithErrors();
+try {
+  await displayAll(db, "SELECT * FROM book");
+} catch (error) {
+  if (error.code === "SQLITE_ERROR") {
+    console.error(error.message);
+  } else {
+    throw error;
+  }
+}
+await runSql(db, "DROP TABLE books");
